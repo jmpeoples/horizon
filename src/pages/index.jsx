8764ab1b41e71eaab5b75cc3fcda00823b36f6ab
jmpeoples <jmpeoples.88@gmail.com>
2023-05-28 23:28:34 -0500

    simple nav

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -261,7 +261,8 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            I’m a designer and a developer. Let me help you bridge the gap
+            between design and tech.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Spencer, a software designer and entrepreneur based in New York
